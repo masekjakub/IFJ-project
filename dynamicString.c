4fef9fd14677b1df60598f9527e1f30c3c1c28e7
Vojtěch Kuchař <vojta.2001.vk@gmail.com>
2022-10-17 18:40:55 +0200

    dynamic string function

---
+++ dynamicString.c
@@ -0,0 +1,19 @@
+/**
+ * @file dynamicString.c
+ * @author Vojtěch Kuchař
+ * @brief Functions operating dynamic string
+ * @version 0.1
+ * @date 2022-10-15
+ */
+
+#include "dynamicString.h"
+
+DynamicString DS_init();
+
+DynamicString DS_apend();
+
+void DS_deleteChar();
+
+void DS_deleteAll();
+
+void DS_dispose();
