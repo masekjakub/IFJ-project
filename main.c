e1fba84df411bbc988cacac332ff7353f32fc239
Filip Polomski <93610939+Filipun@users.noreply.github.com>
2022-10-31 23:20:35 +0100

    update: main.c, scanner.c and added text.php

--- main.c
+++ main.c
@@ -1,10 +1,37 @@
 #include <stdio.h>
+#include "scanner.h"
 
-int main(int argc, char *argv[]){
-    FILE *file = fopen(argv[1],"r");
-    if (file == NULL){
+//  Pro ucely testovani
+int main(int argc, char** argv){
+    FILE *f;
+    Token token;
+    
+    if (argc == 1){
+        fprintf(stderr, "Chybi vstupni soubor\n");
+        return ERR_OTHER;
     }
-    if (!analyse){
+    if ((f = fopen(argv[1], "r")) == NULL){
+        fprintf(stderr, "Soubor neslo otevrit\n");
+        return ERR_OTHER;
+    }
 
+    setSourceFile(f);
+    for (int i = 0; i < 51; i++){
+        token = getToken();
+        if (token.type == TYPE_STRING || token.type == TYPE_ID || token.type == TYPE_FUNID){
+            printf("%d --- %s\n",token.type  ,token.attribute.dString->string);
+            DS_dispose(token.attribute.dString);
+        }
+        else if (token.type == TYPE_INT){
+            printf("%d --- %d\n",token.type ,token.attribute.intV);
+        }
+        else if (token.type == TYPE_DOUBLE){
+            printf("%d --- %f\n",token.type ,token.attribute.doubleV);
+        }
+        else{
+            printf("%d type\n", token.type);
+        }
     }
+
+    fclose(f);
 }
